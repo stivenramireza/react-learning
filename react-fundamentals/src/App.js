f80902018549aa2f63676ee08b6ad51193bf8ba3
Stiven Ramírez Arango <stivenramirez@habi.co>
2022-09-04 21:26:33 -0500

    [ADD] React Learning

---
+++ react-fundamentals/src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Navigation from './components/Navigation';
+import Form from './components/TodoForm';
+
+import { todos } from './todos.json';
+console.log(todos);
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  removeToDo(index) {
+    if(window.confirm('Are you sure you want to delete it?')) {
+      this.setState({
+        todos: this.state.todos.filter((e, i) => {
+          return i !== index
+        })
+      })
+    }
+  }
+
+  render(){
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{ todo.title }</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                { todo.priority }
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{ todo.description }</p>
+              <p><mark>{ todo.responsible }</mark></p>
+            </div>
+            <div className="card-footer">
+              <button 
+                className="btn btn-danger"
+                onClick={this.removeToDo.bind(this, i)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      <div className="App">
+        
+        {/* Header */}
+        <div id="header">
+          <Navigation title="Tasks" counter={ this.state.todos.length } />
+        </div>
+        {/* End Header */}
+
+        {/* Form */}
+        <div className="container">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="container todo-form-container">
+            <Form onAddTodo={this.handleAddTodo} />
+          </div>
+        </div>
+        {/* End Form */}
+
+        {/* Tasks List */}
+        <div className="container">
+          <div className="row mt-4">
+            { todos }
+          </div>  
+        </div>
+        
+        {/* End Tasks List */}
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-redux-2/src/components/MainComponent.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import Menu from '../components/MenuComponent';
+import Header from '../components/HeaderComponent';
+import Footer from '../components/FooterComponent';
+import Home from '../components/HomeComponent';
+import Contact from '../components/ContactComponent';
+import DishDetail from '../components/DishdetailComponent';
+import About from '../components/AboutComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
+        }
+    }
+
+    render(){
+        const HomePage = () => {
+            return(
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            )
+        }
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail 
+                    selectedDish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId))}
+                />
+            );
+        }
+
+        return(
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} /> } />
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default Main;
